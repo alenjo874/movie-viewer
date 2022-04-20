da2d7d565b64134a8e5069e882f1742140cefaf6
Alen Jo <alenjo874@gmail.com>
2022-04-20 14:16:48 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "./style/style.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/home/Home";
-import NavBar from "./components/navigation/NavBar"
-import Movies from "./components/movies/Movies"
-import TvSeries from "./components/tvSeries/TvSeries"
-
+import NavBar from "./components/navigation/NavBar";
+import Movies from "./components/movies/Movies";
+import TvSeries from "./components/tvSeries/TvSeries";
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
+      <NavBar />
       <Switch>
-        <NavBar />
         <Route exact path="/">
           <Home />
         </Route>
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div className="nav-container">
+      <div>Logo</div>
+      <ul className="nav-elements">
+        <li>
+          <Link to="/" >Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+        <li>
+          <Link to="/tv_series">TV Series</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
