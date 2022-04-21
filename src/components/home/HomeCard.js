9d588b5e395b1e1529c98093fd99ae22ae7fc2bc
Alen Jo <alenjo874@gmail.com>
2022-04-21 15:40:57 -0400

    state img backdrop

--- src/components/home/HomeCard.js
+++ src/components/home/HomeCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function HomeCard({backdrop_path}) {
   return (
     <div>
-      <div>
+      <div className="home-backdrop" >
         <img src={`https://image.tmdb.org/t/p/original${backdrop_path}`} />
       </div>
     </div>
